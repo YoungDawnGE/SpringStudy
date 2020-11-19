d89948d7f566659b90ae5dfce217429672c054e0
ge yangchen <geyangchengyc@163.com>
2020-11-19 11:10:54 +0800

    add GYCSpring01  src/test/java

---
+++ GYCSpring01/src/test/java/MyDispatcherServletTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created by GYC
+ * 2020/6/18 0:08
+ */
+public class MyDispatcherServletTest {
+    @Test
+    public void testInit(){
+//        MyDispatcherServlet myDispatcherServlet = new MyDispatcherServlet();
+//        myDispatcherServlet.init();
+
+
+    }
+
+}
